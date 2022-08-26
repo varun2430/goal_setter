3e4b36975e5b283b48b643a84aa334e02b808660
Varun Kadkade <varunkadkade2442@gmail.com>
2022-08-26 21:57:54 +0530

    minor

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus:200,
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
-connectDB()
+app.use(errorHandler);
 
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-app.use(errorHandler)
-app.use(cors(corsOptions))
-
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use('/api/user', require('./routes/userRoutes'))
-
-app.listen(port, () => console.log(`server started on port ${port}`))
+app.listen(port, () => console.log(`server started on port ${port}`));
