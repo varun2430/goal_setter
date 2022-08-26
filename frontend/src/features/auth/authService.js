41f10162ff22cdfe2b5b5502c17b2f6ac6c3f7ee
Varun Kadkade <varunkadkade2442@gmail.com>
2022-08-26 21:31:12 +0530

    minor changes

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,35 +1,35 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api/user/'
+const API_URL = "http://localhost:5000/api/user/";
 
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data
-}
+  return response.data;
+};
 
 const login = async (userData) => {
-    const response = await axios.post(API_URL + 'login', userData)
+  const response = await axios.post(API_URL + "login", userData);
 
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data
-}
+  return response.data;
+};
 
 const logout = () => {
-    localStorage.removeItem('user')
-}
+  localStorage.removeItem("user");
+};
 
 const authService = {
-    register,
-    logout,
-    login,
-}
+  register,
+  logout,
+  login,
+};
 
-export default authService
+export default authService;
--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,50 +1,47 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api/goals/'
+const API_URL = "http://localhost:5000/api/goals/";
 
 const createGoal = async (goalData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    }
+  const response = await axios.post(API_URL, goalData, config);
 
-    const response = await axios.post(API_URL, goalData, config)
-
-    return response.data
-}
+  return response.data;
+};
 
 const getGoals = async (token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    }
+  const response = await axios.get(API_URL, config);
 
-    const response = await axios.get(API_URL, config)
-
-    return response.data
-}
+  return response.data;
+};
 
 const deleteGoal = async (goalId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 
-    }
+  const response = await axios.delete(API_URL + goalId, config);
 
-    const response = await axios.delete(API_URL + goalId, config)
-
-    return response.data
-}
+  return response.data;
+};
 
 const goalService = {
-    createGoal,
-    getGoals,
-    deleteGoal,
-}
+  createGoal,
+  getGoals,
+  deleteGoal,
+};
 
-export default goalService
+export default goalService;
